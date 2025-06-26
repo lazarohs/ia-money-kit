ab36a07ad29106ead82a0299ab3c822612c70330
lazarohs <lazaro.criacao@gmail.com>
2025-06-25 23:43:52 -0300

    Add files via upload

---
+++ ia-money-kit-react/src/App.js
@@ -0,0 +1,11 @@
+
+import React from "react";
+
+export default function App() {
+  return (
+    <div style={{ padding: 40, textAlign: "center" }}>
+      <h1>IA Money Kit</h1>
+      <p>Plataforma React funcionando com sucesso.</p>
+    </div>
+  );
+}
